dce48720753850cd832ceda3f2ade500959c76a2
simgreci <simgreci@microsoft.com>
2016-02-17 11:27:59 +0100

    Test

--- SQLXEtoEventHub/Program.cs
+++ SQLXEtoEventHub/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace SQLXEtoEventHub
 {
@@ -10,6 +11,21 @@ namespace SQLXEtoEventHub
     {
         static void Main(string[] args)
         {
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+            sb.DataSource = "localhost";
+            sb.IntegratedSecurity = true;
+            SqlConnection conn = new SqlConnection(sb.ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandTimeout = 0;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT type, data FROM sys.fn_MSxe_read_event_stream ('GetEvents', 0)";
+            var reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader[1].ToString());
+            }
         }
     }
 }
