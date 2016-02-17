8ba69f7130c99403b9eb594e3080f28187f4bb05
Francesco Cogno <francesco.cogno@outlook.com>
2016-02-17 11:49:13 +0100

    Completed ignore

---
+++ README.md
@@ -0,0 +1,2 @@
+# SQLXEtoEventHub
+Collect Extended Event and send to Event Hub namespace
---
+++ SQLXEtoEventHub/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace SQLXEtoEventHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder sb = new SqlConnectionStringBuilder();
+            sb.DataSource = "localhost";
+            sb.IntegratedSecurity = true;
+            SqlConnection conn = new SqlConnection(sb.ConnectionString);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandTimeout = 0;
+            cmd.CommandType = System.Data.CommandType.Text;
+            cmd.CommandText = "SELECT type, data FROM sys.fn_MSxe_read_event_stream ('GetEvents', 0)";
+            var reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader[1].ToString());
+            }
+        }
+    }
+}
