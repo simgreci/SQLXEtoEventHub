b78620e3a28d8225ebb23b9790d55493079f3caa
simgreci <simgreci@microsoft.com>
2016-02-18 14:51:58 +0100

    Added XESession class

---
+++ SQLXEtoEventHubSp/XESession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLXEtoEventHubSp
+{
+    public class XETrace
+    {
+        public string Name { get; private set; }
+        public string FilePath { get; private set; }
+
+        public void XESession(string name, string filePath)
+        {
+            this.Name = name;
+            this.FilePath = filePath;
+        } 
+    }
+}
