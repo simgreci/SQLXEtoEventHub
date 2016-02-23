ff967cd97da68b927323d6b48a53011541eec599
simgreci <simgreci@microsoft.com>
2016-02-23 17:23:28 +0100

    refined setup.sql

--- SQLXEtoEventHubSp/sql/setup.sql
+++ SQLXEtoEventHubSp/sql/setup.sql
@@ -1,12 +1,21 @@
 ﻿/*xetohub database setup script*/
+use master
+go
 
 if not exists(select name from sys.databases where name = 'xetohub')
 	create database xetohub
 go
 
-alter database xetohub set trustworthy on;
-create assembly System_Runtime_Serialization from 
-'C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Runtime.Serialization.dll'
-with permission_set = unsafe;
+alter database xetohub set trustworthy on
+go
 
-create assembly xetohub from 'c:\sqlserver\clr\sqlxetoeventhubsp.dll' with permission_set = unsafe;
+use xetohub
+go
+
+create assembly [System.Runtime.Serialization] from 
+'C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Runtime.Serialization.dll'
+with permission_set = unsafe
+go
+
+create assembly xetohub from 'c:\sqlserver\clr\sqlxetoeventhubsp.dll' with permission_set = unsafe
+go
