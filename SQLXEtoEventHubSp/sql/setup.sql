4d92a74e53420c5aa83275649786470e907f9944
simgreci <simgreci@microsoft.com>
2016-02-23 17:20:41 +0100

    create setup.sql script to create database

---
+++ SQLXEtoEventHubSp/sql/setup.sql
@@ -0,0 +1,12 @@
+﻿/*xetohub database setup script*/
+
+if not exists(select name from sys.databases where name = 'xetohub')
+	create database xetohub
+go
+
+alter database xetohub set trustworthy on;
+create assembly System_Runtime_Serialization from 
+'C:\Windows\Microsoft.NET\Framework\v4.0.30319\System.Runtime.Serialization.dll'
+with permission_set = unsafe;
+
+create assembly xetohub from 'c:\sqlserver\clr\sqlxetoeventhubsp.dll' with permission_set = unsafe;
