bbd892e6d7c3fb00826f87627e10dae0ca8af398
simgreci <simgreci@microsoft.com>
2016-02-25 22:10:03 +0100

    Fixed SqlStore class

---
+++ xetohub.test/xetohub.core.store.sqlstore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using xetohub.core.store;
+using xetohub.core;
+using xetohub.core.xevent;
+
+namespace xetohub.test
+{
+    [TestClass]
+    public class SqlStore_Test
+    {
+        [TestMethod]
+        public void Test_SqlStore()
+        {
+            DatabaseContext context = new DatabaseContext(Parameters.CONNECTION_STRING);
+
+            using (context)
+            {
+                SqlStore store = new SqlStore(context, "dummy_session");
+                XEPosition dummyPositionIn = new XEPosition();
+                dummyPositionIn.LastFile = "C:\\dummy_path\\dummy_file.xel";
+                dummyPositionIn.Offset = Int64.MaxValue;
+                store.Update(dummyPositionIn);
+                XEPosition dummyPositionOut = store.Read();
+                Assert.AreEqual(dummyPositionIn.LastFile, dummyPositionOut.LastFile);
+                Assert.AreEqual(dummyPositionIn.Offset, dummyPositionOut.Offset);
+            }
+        }
+    }
+}
--- xetohub/core/store/SqlStore.cs
+++ xetohub/core/store/SqlStore.cs
@@ -14,51 +14,48 @@ namespace xetohub.core.store
             _sqlContext = context;
             _sessionName = sessionName;
         }
-        XEPosition IStore.Read()
+        public XEPosition Read()
         {
-            using (SqlConnection conn = _sqlContext.Connection)
+            using (SqlCommand cmd = new SqlCommand())
             {
-                using (SqlCommand cmd = new SqlCommand())
+                cmd.Connection = _sqlContext.Connection;
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.CommandText = "xetohub.dbo.xetohub_read_offset";
+                SqlParameter param = new SqlParameter("session_name", SqlDbType.NVarChar, 256);
+                param.Value = _sessionName;
+                cmd.Parameters.Add(param);
+                SqlDataReader reader = cmd.ExecuteReader();
+                XEPosition pos = new XEPosition();
+                if (reader.HasRows)
                 {
-                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                    cmd.CommandText = "dbo.xetohub_read_offset";
-                    SqlParameter param = new SqlParameter("session_name", SqlDbType.NVarChar, 256);
-                    param.Value = _sessionName;
-                    cmd.Parameters.Add(param);
-                    SqlDataReader reader = cmd.ExecuteReader();
-                    XEPosition pos = new XEPosition();
-                    if(reader.HasRows)
-                    {
-                        pos.LastFile = reader["session_file_name"].ToString();
-                        pos.Offset = Convert.ToInt64(reader["session_offset"]);
-                    }
-                    return pos;
+                    reader.Read();
+                    pos.LastFile = reader["session_file_name"].ToString();
+                    pos.Offset = Convert.ToInt64(reader["session_offset"]);
                 }
+                return pos;
             }
         }
 
-        void IStore.Update(XEPosition pos)
+        public void Update(XEPosition pos)
         {
-            using (SqlConnection conn = _sqlContext.Connection)
+            using (SqlCommand cmd = new SqlCommand())
             {
-                using (SqlCommand cmd = new SqlCommand())
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.CommandText = "dbo.xetohub_save_or_update_offset";
-                    SqlParameter param = new SqlParameter("session_name", SqlDbType.NVarChar, 256);
-                    param.Value = _sessionName;
-                    cmd.Parameters.Add(param);
+                cmd.Connection = _sqlContext.Connection;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "xetohub.dbo.xetohub_save_or_update_offset";
+                SqlParameter param = new SqlParameter("session_name", SqlDbType.NVarChar, 256);
+                param.Value = _sessionName;
+                cmd.Parameters.Add(param);
 
-                    SqlParameter param1 = new SqlParameter("session_file_name", SqlDbType.NVarChar, 260);
-                    param1.Value = pos.LastFile;
-                    cmd.Parameters.Add(param1);
+                SqlParameter param1 = new SqlParameter("session_file_name", SqlDbType.NVarChar, 260);
+                param1.Value = pos.LastFile;
+                cmd.Parameters.Add(param1);
 
-                    SqlParameter param2 = new SqlParameter("session_offset", SqlDbType.BigInt);
-                    param2.Value = pos.Offset;
-                    cmd.Parameters.Add(param2);
+                SqlParameter param2 = new SqlParameter("session_offset", SqlDbType.BigInt);
+                param2.Value = pos.Offset;
+                cmd.Parameters.Add(param2);
 
-                    cmd.ExecuteNonQuery();
-                }
+                cmd.ExecuteNonQuery();
             }
         }
     }
